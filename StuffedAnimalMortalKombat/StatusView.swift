8b425ec3e30bb6854ab8322e6f8776c3f07d5cca
Sinkingstarship <kylerobinson1988@gmail.com>
2015-06-11 23:34:12 -0400

    Final Thursday Changes.
    Final Thursday changes.

--- StuffedAnimalMortalKombat/StatusView.swift
+++ StuffedAnimalMortalKombat/StatusView.swift
@@ -8,44 +8,121 @@
 
 import UIKit
 
-@IBDesignable class StatusView: UIView {
-    
-  
 
-}
-
-class StatusBars: UIView {
+@IBDesignable class StatusBars: UIView {
     
     @IBInspectable var energyPoints: CGFloat = 90
-    
     @IBInspectable var barColor: UIColor = UIColor.greenColor()
-    
-    @IBInspectable var lightBarColor: UIColor = UIColor.lightGrayColor()
+    @IBInspectable var lightBarColor: UIColor = UIColor.blueColor()
     
     override func drawRect(rect: CGRect) {
         
         let context = UIGraphicsGetCurrentContext()
+        let barRect = CGRectInset(rect, 1, 1)
+        let path = UIBezierPath(roundedRect: barRect, cornerRadius: 10)
         
         let lineWidth: CGFloat = 5
         CGContextSetLineWidth(context, lineWidth)
         
-        //Line width or bar width
-        let barRect = CGRectInset(rect, lineWidth/2, lineWidth/2)
+        lightBarColor.set()
+        CGContextAddPath(context, path.CGPath)
+        CGContextFillPath(context)
         
-        //Line dot
-//        addDotAtDegrees(-45, withColor: lightEnergy, andContext: context)
-//        addDotAtDegrees(45, withColor: energyColor, andContext: context)
-        
+        barColor.set()
         CGContextSetBlendMode(context, kCGBlendModeSourceIn)
-        let healthY = (1.0 - (healthPoints / 100)) * rect.height
-        
-        energyColor.set()
-        
-        CGContextMoveToPoint(context, x, y)
-        CGContextAddLineToPoint(context, rect.width/2, rect.height/2)
-        CGContextAddLineToPoint(context, 0, rect.height)
+        CGContextFillRect(context, CGRectMake(0, 0, rect.width - energyPoints, rect.height))
         CGContextFillPath(context)
         
     }
     
+}
+
+
+@IBDesignable class LivesDisplay: UIView {
+    
+    @IBInspectable var livesLeft: Int = 3 {
+        
+        didSet {
+            
+            setNeedsDisplay()
+        }
+    }
+    
+    @IBInspectable var padding: CGFloat = 3
+    
+    override func drawRect(rect: CGRect) {
+        
+        backgroundColor = UIColor.clearColor()
+        var context = UIGraphicsGetCurrentContext()
+        
+        var height = rect.height * 0.8
+        var width = rect.height * 0.8
+        
+        for i in 0..<livesLeft {
+            
+            let x = CGFloat(i) * (width + padding)
+            let y: CGFloat = 3
+            
+            UIColor.purpleColor().set()
+            
+            CGContextFillEllipseInRect(context, CGRectMake(x, y, width, height))
+            
+            
+        }
+    
+    }
+
+
+}
+
+@IBDesignable class KillsDisplay: UIView {
+    
+    @IBInspectable var killCount: Int = 12 {
+        
+        didSet {
+            
+            setNeedsDisplay()
+        }
+    }
+    
+    @IBInspectable var padding: CGFloat = 3
+    
+    override func drawRect(rect: CGRect) {
+        
+        backgroundColor = UIColor.clearColor()
+        var context = UIGraphicsGetCurrentContext()
+        
+        var height = rect.height * 0.085
+        var width = rect.height * 0.085
+        if (killCount < 9) {
+            
+            for i in 0..<killCount {
+                
+                let x = CGFloat(i) * (width + padding)
+                let y: CGFloat = 3
+                
+                UIColor.redColor().set()
+                
+                CGContextFillEllipseInRect(context, CGRectMake(x, y, width, height))
+                
+                
+            }
+            
+        } else if (killCount > 9) {
+                
+                for i in 0..<(killCount - 8) {
+                    
+                    let x = CGFloat(i) * (width + padding)
+                    let y: CGFloat = 5
+                    
+                    CGContextFillEllipseInRect(context, CGRectMake(x, y, width, height))
+            
+        }
+        
+        
+    }
+    
+    
+}
+
 }
